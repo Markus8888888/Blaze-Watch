dc9988bd0b8f5f07fa0a1861f1e51b829b79fbe1
Alex <alexxu0308@gmail.com>
2025-06-29 19:59:40 -0600

    updated routing

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,17 +2,100 @@ from app import app
 from flask import request, jsonify, render_template
 import pickle
 import numpy as np
+import openmeteo_requests
+import requests_cache
+import requests
 
 spreadModel = pickle.load(open("spread_model.pkl", "rb"))
 
+cache_sess = requests_cache.CachedSession('.cache', expire_after=5)
+om = openmeteo_requests.Client(session=cache_sess)
+
+# set up vegetation data
+with open("./training/data_2024/Vegetation.txt", "r") as f:
+    lines = f.readlines()
+
+data = []
+
+for line in lines:
+    try:
+        nums = [int(val) for val in line.strip().split()]
+        data.extend(nums)
+    except ValueError:
+        continue
+
+veg_array = np.array(data[:180 * 360]).reshape((180, 360))
+
+def getWeather(lat, lon):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "current": ["temperature_2m", "relative_humidity_2m", "wind_speed_10m", "wind_direction_10m", "precipitation"],
+    }
+    responses = om.weather_api(url, params=params)
+    resp = responses[0]
+    current = resp.Current()
+    
+    vals = [current.Variables(i).Value() for i in range(current.VariablesLength())]
+    return {
+        "temperature_2m": vals[0],
+        "relative_humidity_2m": vals[1],
+        "wind_speed_10m": vals[2],
+        "wind_direction_10m": vals[3],
+        "precipitation": vals[4],
+    }
+
+def getVegetation(lat, lon):
+    convertedLat = int(lat + 89.5)  
+    convertedLon = int(lon + 180)    
+
+    return veg_array[convertedLat, convertedLon]
+
+# routes
 @app.route('/predict-spread', methods=['POST'])
 def predictSpread():
     coords = request.json
-    lonMax = coords.get("lon_max")
-    lonMin = coords.get("lon_min")
-    latMax = coords.get("lat_max")
-    latMin = coords.get("lat_min")
-    X = np.array([[latMax, latMin, lonMax, lonMin]])
+    lonMaxLat = coords.get("lon_max_lat")
+    lonMaxLon = coords.get("lon_max_lon")
+    lonMinLat = coords.get("lon_min_lat")
+    lonMinLon = coords.get("lon_min_lon")
+    latMaxLat = coords.get("lat_max_lat")
+    latMaxLon = coords.get("lat_max_lon")
+    latMinLat = coords.get("lat_min_lat")
+    latMinLon = coords.get("lat_min_lon")
+
+    points = {
+        "LAT_MAX": (latMaxLat, latMaxLon),
+        "LAT_MIN": (latMinLat, latMinLon),
+        "LON_MAX": (lonMaxLat, lonMaxLon),
+        "LON_MIN": (lonMinLat, lonMinLon)
+    }
+
+    predictors = []
+
+    predictors.extend([
+        latMaxLat, latMaxLon,
+        latMinLat, latMinLon,
+        lonMaxLat, lonMaxLon,
+        lonMinLat, lonMinLon
+    ])
+
+    for key, (lat, lon) in points.items():
+        weather = getWeather(lat, lon)
+        predictors.extend([
+            weather["temperature_2m"],
+            weather["relative_humidity_2m"],
+            weather["wind_speed_10m"],
+            weather["wind_direction_10m"],
+            weather["precipitation"]
+        ])
+    
+    for key, (lat, lon) in points.items():
+        predictors.append(getVegetation(lat, lon))
+
+    X = np.array([predictors])
+    print(X)
 
     prediction = spreadModel.predict(X)
     return jsonify({'predictions': prediction.tolist()})
